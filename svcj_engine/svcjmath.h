863ece49b6ccc09dae8bfd3e909ee21ef284ddd5
N2304862K <n2304862k@e.ntu.edu.sg>
2025-11-26 00:36:02 +0800

    Refactor function declarations in svcjmath.h

--- svcj_engine/svcjmath.h
+++ svcj_engine/svcjmath.h
@@ -1,6 +1,23 @@
+// svcj_fast_factors/src/svcjmath.h
 #ifndef SVCJMATH_H
 #define SVCJMATH_H
+
 #define NUM_PARAMS 8
-void svcj_single_step_estimate_qmle(const double*, int, const double*, double*);
-int svcj_full_rolling_fit_2d(const double*, int, int, int, int, double*);
+
+void svcj_single_step_estimate_qmle(
+    const double* returns_array, 
+    int N, 
+    const double* initial_params, 
+    double* estimated_params
+);
+
+int svcj_full_rolling_fit_2d(
+    const double* full_returns_matrix,
+    int total_T,
+    int total_A,
+    int window_size,
+    int step_size,
+    double* output_drift_tensor
+);
+
 #endif
