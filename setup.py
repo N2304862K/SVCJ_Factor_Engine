b3c9e3db1ca6f692facce10e4e30d370c01ceba1
N2304862K <n2304862k@e.ntu.edu.sg>
2025-11-26 00:49:09 +0800

    Update setup.py for version 0.2.0

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-# svcj_fast_factors/setup.py
+# setup.py
 from setuptools import setup, Extension
-from Cython.Build import cythonize
 import numpy
 
 extensions = [
@@ -18,27 +17,24 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='svcj_fast_factors',
-    version='0.1.1', 
+    version='0.2.0', 
     author="N2304862K", 
     description="A high-performance C/Cython SVCJ factor generation library.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/N2304862K/SVCJ_Factor_Engine",
     
-    ext_modules=cythonize(extensions),
+    ext_modules=extensions,
+    
     install_requires=[
         "numpy>=1.20",
-        "pandas>=1.3",
-        "yfinance>=0.1.70",
-        "Cython>=0.29"
+        "pandas>=1.3"
     ],
     
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Financial and Insurance Industry"
     ],
     python_requires='>=3.8',
 )
