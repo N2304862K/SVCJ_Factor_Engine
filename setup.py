950132022028b1dcabec2dccd41e9f5b88ec6341
N2304862K <n2304862k@e.ntu.edu.sg>
2025-11-26 00:33:21 +0800

    Refactor setup.py to simplify extension definition

--- setup.py
+++ setup.py
@@ -1,45 +1,26 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-extensions = [
-    Extension(
-        name="svcj_engine.svcj_wrapper", 
-        
-        sources=[
-            "svcj_engine/svcj_wrapper.pyx", 
-            "svcj_engine/svcjmath.c"
-        ], 
-        include_dirs=[numpy.get_include()],
-        extra_compile_args=["-O3"] 
-    )
-]
+extensions = [Extension("svcj_wrapper", sources=["svcj_wrapper.pyx", "svcjmath.c"], include_dirs=[numpy.get_include()], extra_compile_args=["-O3"])]
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='svcj-factor-engine',
-    
     version='1.0.0',
-    
     author='JTPC',
     author_email='tpcheung@gmail.com',
-    
     description='A high-performance engine for generating SVCJ factors.',
-    
     long_description=long_description,
     long_description_content_type="text/markdown",
-    
     url='https://github.com/N2304862K/SVCJ_Factor_Engine.git',
-    
     packages=find_packages(),
-    
     ext_modules=cythonize(extensions),
     install_requires=[
         'numpy',
-        'pandas',
-        'yfinance'
+        'pandas'
     ],
     
     setup_requires=[
