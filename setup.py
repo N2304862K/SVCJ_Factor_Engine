c4a49ac5ecadecf900add66039b0090767466308
N2304862K <n2304862k@e.ntu.edu.sg>
2025-11-26 10:30:13 +0000

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-# File: setup.py
-# This script defines the package and its compiled extensions.
-# It is designed to be "NumPy-aware" by deferring the import of numpy.
+# File: setup.py (The Definitive Solution)
 
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
-# A small trick to defer the numpy import until after it has been
-# installed by the build backend (pip).
-class get_numpy_include:
-    def __str__(self):
+# --- The "Numpy-Aware" Build Extension ---
+# This class ensures that numpy is imported only when it's absolutely needed.
+# It's the canonical way to solve the "numpy.get_include()" problem during
+# a pip install of a Cython package.
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from being imported before it's installed.
+        __builtins__.__NUMPY_SETUP__ = False
         import numpy
-        return numpy.get_include()
+        self.include_dirs.append(numpy.get_include())
 
 # Define the C extension module
 extensions = [
@@ -19,7 +23,7 @@ extensions = [
             "svcj_engine/svcj_wrapper.pyx", 
             "svcj_engine/svcjmath.c"
         ], 
-        include_dirs=[get_numpy_include()],
+        # The include_dirs will be populated by the class above at build time
         extra_compile_args=["-O3"]
     )
 ]
@@ -30,18 +34,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='svcj_factor_engine',
-    version='1.1.0', # It's good practice to bump the version
+    version='1.2.0', # Bump version for the critical build fix
     author='N2304862K',
     description='A high-speed engine for generating SVCJ risk factors from financial time series data.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/N2304862K/SVCJ_Factor_Engine",
     packages=['svcj_engine'],
+    # Tell setuptools to use our custom build_ext class
+    cmdclass={'build_ext': build_ext},
     ext_modules=extensions,
     install_requires=[
         'numpy>=1.20.0',
         'pandas>=1.3.0',
     ],
+    # setup_requires is still a good fallback for some environments
+    setup_requires=['numpy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
