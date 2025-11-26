e04424f11534345f809757ddb4d028b37c0fee38
N2304862K <n2304862k@e.ntu.edu.sg>
2025-11-26 09:44:48 +0000

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,37 @@
-# File: setup.py
+# File: setup.py (Corrected)
 
 from setuptools import setup, Extension
-from Cython.Build import cythonize
+# DO NOT import cythonize here. Let setuptools handle it.
 import numpy
 
-# Define the C extension module that needs to be compiled
+# Define the C extension module. Setuptools will see the ".pyx" extension
+# and know to use Cython to build it.
 extensions = [
     Extension(
-        # The name of the resulting module that will be imported in Python
         "svcj_engine.svcj_wrapper", 
-        # List of all source files required for compilation
         sources=[
             "svcj_engine/svcj_wrapper.pyx", 
             "svcj_engine/svcjmath.c"
         ], 
-        # Include directory for NumPy headers, necessary for Cython
         include_dirs=[numpy.get_include()],
-        # Compiler flags for maximum optimization
         extra_compile_args=["-O3"]
     )
 ]
 
 setup(
     name='svcj_factor_engine',
-    version='1.0.0',
+    version='1.0.1', # Bump version for new release
     author='N2304862K',
     description='A high-speed engine for generating SVCJ risk factors from financial time series data.',
-    # Specify the packages to be included in the distribution
     packages=['svcj_engine'],
-    # Tell setuptools to use Cython to build the extensions
-    ext_modules=cythonize(extensions),
-    # Define dependencies required for the package to run
+    # Simply pass the list of extensions. setuptools handles the rest.
+    ext_modules=extensions,
     install_requires=[
         'numpy>=1.20.0',
         'pandas>=1.3.0',
     ],
+    # This keyword is a fallback for older versions of pip/setuptools
+    # to ensure they install Cython before trying to build.
+    setup_requires=['cython', 'numpy'],
     zip_safe=False,
 )
